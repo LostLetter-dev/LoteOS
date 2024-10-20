ef42b8286136e78cc8c40af86327c66335c8e1d9
Lost Letter <163488754+LostLetter-dev@users.noreply.github.com>
2024-10-20 12:27:28 +0100

    Create display_manager.h

---
+++ kernel/display_manager.h
@@ -0,0 +1,9 @@
+// display_manager.h
+#ifndef DISPLAY_MANAGER_H
+#define DISPLAY_MANAGER_H
+
+void clear_screen();
+void print_char(char c);
+void print_string(const char* str);
+
+#endif // DISPLAY_MANAGER_H
