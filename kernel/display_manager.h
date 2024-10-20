31406243fb9403563f745a291ebfd23e5e9708e5
Lost Letter <163488754+LostLetter-dev@users.noreply.github.com>
2024-10-20 14:28:49 +0100

    Update display_manager.h

--- kernel/display_manager.h
+++ kernel/display_manager.h
@@ -6,4 +6,10 @@ void clear_screen();
 void print_char(char c);
 void print_string(const char* str);
 
+void set_resolution(int width, int height);
+void switch_tab(int tab);
+
+void draw_vertical_split();
+void make_tab();
+
 #endif // DISPLAY_MANAGER_H
