b5bd76df96775eefe9a8ed95fb0ad338eeec5b11
Lost Letter <163488754+LostLetter-dev@users.noreply.github.com>
2024-10-20 14:24:12 +0100

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,12 +2,29 @@
 #include "display_manager.h"
 
 void kernel_main(void) {
-    clear_screen();  // Clear the screen
+    set_resolution(80, 25);  // Set screen resolution to 80x25
+    clear_screen();          // Clear the screen
+
     print_string("LoteOS V-0-1");
-    print_string("END MY SUFFERING.");  // Use the display manager to print
-    
+
+    // Dynamically create tabs
+    make_tab();  // Create the first tab
+
+    make_tab();  // Create the second tab
+
+    // Switch between tabs and print
+    switch_tab(0);  // Go back to Tab 1
+    print_string("LoteOS V-0-1");
+    print_string("This shouldn't make any impact on tab 2");
+    print_string("This shouldn't make any impact on tab 2");
+    print_string("This shouldn't make any impact on tab 2");
+    print_string("This shouldn't make any impact on tab 2");
+
+    switch_tab(1);  // Switch to Tab 2
+    print_string("STFU tab 1...");
+
     // Prevent returning to GRUB
     while (1) {
-        __asm__("hlt");  // Halt CPU to save power until the next interrupt
+        __asm__("hlt");  // Halt CPU to save power until the
     }
 }
